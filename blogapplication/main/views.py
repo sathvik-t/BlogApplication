fef2e0048d96b7e18d379a94f2e85dc890f6bc72
Sathvik <satviklca@gmail.com>
2023-11-14 10:43:42 +0530

    Added pagination

--- blogapplication/main/views.py
+++ blogapplication/main/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render
+from typing import Any
+from django.db.models.query import QuerySet
+from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.models import User
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from .models import Post
@@ -15,6 +18,17 @@ class PostListView(ListView):
     template_name = 'main/home.html'
     context_object_name = 'posts'
     ordering = ['-date_posted']
+    paginate_by = 4
+        
+class UserPostListView(ListView):
+    model = Post
+    template_name = 'main/user_posts.html'
+    context_object_name = 'posts'
+    paginate_by = 4
+
+    def get_queryset(self):
+        user = get_object_or_404(User, username=self.kwargs.get('username'))
+        return Post.objects.filter(author=user).order_by('-date_posted')
 
 class PostDetailView(DetailView):
     model = Post
